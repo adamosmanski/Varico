6009b2a3882f1bd992b0f2c2ad84424a6eea6094
Adam Osmański <adamosmanski1@gmail.com>
2025-02-20 18:04:50 +0100

    Create PasswordService to hash password

---
+++ Varico.Core/Services/PasswordService/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace Varico.Core.Services.PasswordService
+{
+    public interface IPasswordService
+    {
+        public bool VerifyPassword(string password, string hash);
+        public string HashPassword(string password);
+    }
+}
