8d949e043928f9549ffcec74b883c3667921646d
Adam Osmański <adamosmanski1@gmail.com>
2025-02-20 18:05:05 +0100

    Init services

--- Varico.Core/Program.cs
+++ Varico.Core/Program.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.EntityFrameworkCore;
+using Varico.Core.Services.PasswordService;
 using Varico.EF.Models;
 
 namespace Varico.Core
@@ -11,13 +12,15 @@ namespace Varico.Core
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            ConfigureServices(builder);
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<VaricoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
+
+            app.UseCors("AllowAll");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -34,6 +37,24 @@ namespace Varico.Core
             app.MapControllers();
 
             app.Run();
+        }
+
+        public static void ConfigureServices(IHostApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<VaricoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200/")
+                           .AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+            builder.Services.AddSingleton<IPasswordService, PasswordService>();
         }
     }
 }
