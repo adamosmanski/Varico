907951ce1f13f8953aa892a0ce4c49eb65fa9d7f
Adam Osmański <adamosmanski1@gmail.com>
2025-02-20 20:07:10 +0100

    Added field to Cars

--- Varico.EF/Migrations/20250220190633_AddedPictureUrlToCars.Designer.cs
+++ Varico.EF/Migrations/20250220190633_AddedPictureUrlToCars.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Varico.EF.Models;
 
@@ -10,9 +11,11 @@ using Varico.EF.Models;
 namespace Varico.EF.Migrations
 {
     [DbContext(typeof(VaricoDbContext))]
-    partial class VaricoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250220190633_AddedPictureUrlToCars")]
+    partial class AddedPictureUrlToCars
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
