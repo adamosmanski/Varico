7350d4403c2809ec344875e8bb723f709cedd639
Adam Osmański <adamosmanski1@gmail.com>
2025-02-20 18:06:04 +0100

    Init UserController

---
+++ Varico.Core/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Varico.Core.CQRS.UserModule;
+using Varico.Core.Services.PasswordService;
+using Varico.EF.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] CreateUserCommand command)
+    {
+        var user = await _mediator.Send(command);
+        return Ok(new { message = "Użytkownik zarejestrowany pomyślnie!", user });
+    }
+
+    [HttpGet("getUser/{email}")]
+    public async Task<IActionResult> GetUserByMail(string email)
+    {
+        var user = await _mediator.Send(new GetUserByEmailQuery(email));
+        if (user == null)
+        {
+            return NotFound(new { message = "Użytkownik nie znaleziony" });
+        }
+        return Ok(user);
+    }
+
+}
