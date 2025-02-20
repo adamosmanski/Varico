f597a6f56c53ab3d438cc3ec01251b745695b9b5
Adam Osmański <adamosmanski1@gmail.com>
2025-02-20 20:00:13 +0100

    Update app.component.ts

--- Varico.UI/src/app/app.component.ts
+++ Varico.UI/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthService } from './services/auth-service.service';
+import { SidebarComponent } from './components/sidebar/sidebar.component'; 
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent], 
-  template: `
-    <app-navbar></app-navbar>
-    <router-outlet></router-outlet>
-  `
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+  }
+}
