466e1079721fe077e1bbd4e12c3dfbae540fe6d4
Adam Osmański <adamosmanski1@gmail.com>
2025-02-21 11:03:44 +0100

    Update vehicle-service.service.ts

--- Varico.UI/src/app/services/vehicle-service.service.ts
+++ Varico.UI/src/app/services/vehicle-service.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Vehicle } from '../models/vehicle.model';
+import { AuthService } from './auth-service.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +11,42 @@ import { Vehicle } from '../models/vehicle.model';
 export class VehicleService {
   private apiUrl = 'https://localhost:7016/api/Vehicles';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private authService: AuthService
+  ) {}
 
   getVehicles(): Observable<Vehicle[]> {
     return this.http.get<Vehicle[]>(`${this.apiUrl}/getAllVehicles`);
+  }
+
+  
+  reserveVehicle(vehicleId: number, reservedFrom: Date, reservedTo: Date, userId: number): Observable<any> {
+    const reservationData = {
+      vehicleId: vehicleId,
+      reservedFrom: reservedFrom,
+      reservedTo: reservedTo,
+      userId: userId
+    };
+    return this.http.post(`${this.apiUrl}/reserveVehicle`, reservationData);
+  }
+
+  getReservedVehicles(): Observable<Vehicle[]> {
+    const userId = this.authService.getUserId();
+    console.log(userId);
+  
+    if (userId === null) {
+      throw new Error('User is not logged in');
+    }
+  
+    return this.http.get<any[]>(`${this.apiUrl}/reserved?userId=${userId}`).pipe(
+      map((vehicles) => vehicles.map(vehicle => ({
+        ...vehicle,
+        reservedFrom: vehicle.dateStart ? new Date(vehicle.dateStart) : null,
+        reservedTo: vehicle.dateEnd ? new Date(vehicle.dateEnd) : null
+      })))
+    );
+  }
+  returnReservedVehicles(vehicleId: number): Observable<void> {
+    return this.http.patch<void>(`${this.apiUrl}/returnReservedVehicle`, { vehicleId });
   }
 }
