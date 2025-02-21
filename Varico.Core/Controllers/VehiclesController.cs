86d7e04009872d1cb59227a6321f2d2e9e42e168
Adam Osmański <adamosmanski1@gmail.com>
2025-02-21 01:13:32 +0100

    Create VehiclesController.cs

---
+++ Varico.Core/Controllers/VehiclesController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Varico.Core.CQRS.CarModule;
+
+namespace Varico.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public VehiclesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("getAllVehicles")]
+        public async Task<IActionResult> GetAllVehicles()
+        {
+            var vehicles = await _mediator.Send(new GetAllVehicleQuery());
+            return Ok(vehicles);
+        }
+    }
+}
