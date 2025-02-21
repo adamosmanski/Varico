d04265db2559134bb6dafa973ad09c6e2d1f16ba
Adam Osmański <adamosmanski1@gmail.com>
2025-02-21 01:14:17 +0100

    Create Vehicle.cs

---
+++ Varico.EF/Models/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace Varico.EF.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Brand { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Model { get; set; }
+
+        [Required]
+        public int ProductionYear { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(VehicleType))]
+        public VehicleType VehicleType { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(BodyType))]
+        public BodyType BodyType { get; set; }
+
+        [Required]
+        public bool Availability { get; set; }
+
+        public int? ReservedById { get; set; }
+
+        [ForeignKey("ReservedById")]
+        public virtual Users? ReservedBy { get; set; }
+
+        [Required]
+        public string? PictureURL { get; set; }
+    }
+
+}
