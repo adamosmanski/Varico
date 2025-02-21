01e9a72d18d50e54acf6698d070bc8ecc6b8414d
Adam Osmański <adamosmanski1@gmail.com>
2025-02-21 01:13:35 +0100

    Create GetAllVehicle.cs

---
+++ Varico.Core/CQRS/CarModule/GetAllVehicle.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Varico.EF.Models;
+
+namespace Varico.Core.CQRS.CarModule
+{
+    public record GetAllVehicleQuery() : IRequest<IEnumerable<Vehicle>>;
+    public class GetAllVehicleHandler : IRequestHandler<GetAllVehicleQuery, IEnumerable<Vehicle>>
+    {
+        private readonly VaricoDbContext _context;
+        public GetAllVehicleHandler(VaricoDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Vehicle>> Handle(GetAllVehicleQuery request, CancellationToken cancellationToken)
+        {
+            var vehicles = await _context.Vehicles.ToListAsync(cancellationToken);
+            return vehicles;
+        }
+    }
+}
