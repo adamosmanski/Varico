7bb6d2197077647a52ff17148f9de388f3610512
Adam Osmański <adamosmanski1@gmail.com>
2025-02-21 04:08:17 +0100

    Update vehicle.model.ts

--- Varico.UI/src/app/models/vehicle.model.ts
+++ Varico.UI/src/app/models/vehicle.model.ts
@@ -3,7 +3,55 @@ export interface Vehicle {
   brand: string;
   model: string;
   productionYear: number;
-  vehicleType: string;
+  vehicleType: 'Sedan' | 'SUV' | 'Hatchback' | 'Coupe' | 'Convertible';
+  bodyType: 'Sedan' | 'SUV' | 'Hatchback' | 'Coupe' | 'Convertible';
   availability: boolean;
+  reservedById?: number | null;
+  reservedBy?: string | null;
   pictureURL: string;
+  mileage: number;
+  transmission: 'Manual' | 'Automatic' | 'Semi-Automatic';  
+  engineCapacity: number;
+  price: number;
+  fuelType: 'Petrol' | 'Diesel' | 'Electric' | 'Hybrid';
+  drivetrain: 'Front-Wheel' | 'Rear-Wheel' | 'All-Wheel';
+  acceleration: number;
+  trunkCapacity: number;
+  maxSpeed: number; 
 }
+
+export enum VehicleType {
+  Sedan = 'Sedan',
+  SUV = 'SUV',
+  Hatchback = 'Hatchback',
+  Coupe = 'Coupe',
+  Convertible = 'Convertible',
+}
+
+export enum BodyType {
+  Sedan = 'Sedan',
+  SUV = 'SUV',
+  Hatchback = 'Hatchback',
+  Coupe = 'Coupe',
+  Convertible = 'Convertible',
+}
+
+export enum TransmissionType {
+  Manual = 'Manual',
+  Automatic = 'Automatic',
+  SemiAutomatic = 'Semi-Automatic',
+}
+
+export enum FuelType {
+  Petrol = 'Petrol',
+  Diesel = 'Diesel',
+  Electric = 'Electric',
+  Hybrid = 'Hybrid',
+}
+
+export enum DrivetrainType {
+  FrontWheel = 'Front-Wheel',
+  RearWheel = 'Rear-Wheel',
+  AllWheel = 'All-Wheel',
+}
+
