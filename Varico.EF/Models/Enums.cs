5864d27bbe8fe7bd067bff1dbacea8535594c50d
Adam Osmański <adamosmanski1@gmail.com>
2025-02-21 04:07:38 +0100

    Create Enums.cs

---
+++ Varico.EF/Models/Enums.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varico.EF.Models
+{
+    public enum FuelType
+    {
+        Petrol,
+        Diesel,
+        Electric,
+        Hybrid,
+        Gas
+    }
+    public enum VehicleType
+    {
+        Passenger,
+        Truck,
+        Motorcycle
+    }
+
+    public enum BodyType
+    {
+        SUV,
+        HATCHBACK,
+        SEDAN,
+        KOMBI,
+        ANOTHER
+    }
+    public enum TransmissionType
+    {
+        Manual,
+        Automatic
+    }
+    public enum DrivetrainType
+    {
+        FWD,
+        RWD,
+        AWD //4x4
+    }
+}
